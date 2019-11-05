5e1638de442ad07dea19e190bd30efe012ddf842
Andre Rossi <andre.rossi@unesp.br>
2019-11-05 19:26:27 -0300

    bug fixes in main and graph change

--- main_KF.py
+++ main_KF.py
@@ -34,11 +34,6 @@ learning_rate_init= 0.01
 # In[8]:
 
 
-acur_RNA= []
-acur_SVM= []
-
-acurB_RNA= []
-acurB_SVM= []
 
 media_SVM= []
 des_SVM= []
@@ -55,60 +50,76 @@ matrizC_S= []
 
 extractor_name= []
 
-
 # In[9]:
 
 
 KF= KFold(n_splits=10, random_state=13, shuffle=True) #(n_folds, semente, ordenacao)
 
 for nome in listdir(diretorio):
+    acur_RNA= []
+    acur_SVM= []
+
+    acurB_RNA= []
+    acurB_SVM= []
+
+    matrizC_RNA= []
+    matrizC_SVM= []
+
     dados= genfromtxt((diretorio+nome), delimiter=',',skip_header=1)
 
     classCol = dados.shape[1] - 1
     y = dados[:,classCol]
     x = dados[:, :(classCol-1)]
-    
+
     KF_data = pd.DataFrame(KF.split(x)) #Lendo os valores p/ exportacao em .csv
-    KF_data.to_csv('kfolds_data.csv') #Salvando em .csv 
-    
-    matrizC_RNA= []
-    matrizC_SVM= []
-    
+    KF_data.to_csv('kfolds_data.csv') #Salvando em .csv
+
     for train_index, test_index in KF.split(x):
         x_train, x_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        
+
         #Treinando e avaliando o desempenho da SVM
         S= svm_andre.SVM(x_train, y_train, x_test, y_test, C, gamma, kernel)
         acur_SVM.append(S[0]) #Lista de acuracias de todos os folds
         acurB_SVM.append(S[1]) #Lista de acuracias balanceadas de todos os folds
         matrizC_SVM.append(S[2]) #Lista de matrizes de confusao de todos os folds
-        
+
         #Treinando e avaliando o desempenho da RNA
         R= rna_mlp.RNAs(x_train, y_train, x_test, y_test, learning_rate_init,
                         learning_rate, hidden_layer_sizes, max_iter)
         acur_RNA.append(R[0]) #Lista de acuracias de todos os folds
         acurB_RNA.append(R[1]) #Lista de acuracias balanceadas de todos os folds
         matrizC_RNA.append(R[2]) #Lista de matrizes de confusao de todos os folds
-    
+
     media_SVM.append(statistics.mean(acur_SVM))
     des_SVM.append(statistics.stdev(acur_SVM))
     media_RNA.append(statistics.mean(acur_RNA))
     des_RNA.append(statistics.stdev(acur_RNA))
-    
+
     mediaB_SVM.append(statistics.mean(acurB_SVM))
     desB_SVM.append(statistics.stdev(acurB_SVM))
     mediaB_RNA.append(statistics.mean(acurB_RNA))
     desB_RNA.append(statistics.stdev(acurB_RNA))
-    
+
     matrizC_S.append(np.sum(matrizC_SVM, axis=0))
     matrizC_R.append(np.sum(matrizC_RNA, axis=0))
-    
+
     extractor_name.append(nome[:-4])
 
 
 # In[10]:
 
+#w = pd.DataFrame(media_SVM)
+#w.to_csv("media")
+
+acc_std_models = {'extractorName' : extractor_name,
+    'acc_SVM': media_SVM, 'stdev_acc_SVM': des_SVM, 'acc_RNA': media_RNA, 'stdev_acc_RNA': des_RNA,
+    'acc_balanced_SVM' : mediaB_SVM, 'stdev_acc_balanced_SVM': desB_SVM, 'acc_balanced_RNA' : mediaB_RNA, 'stdev_acc_balanced_SVM': desB_RNA
+}
+df = pd.DataFrame(acc_std_models, columns= ['extractorName', 'acc_SVM', 'stdev_acc_SVM', 'acc_RNA', 'stdev_acc_RNA',
+    'acc_balanced_SVM', 'stdev_acc_balanced_SVM', 'acc_balanced_RNA', 'stdev_acc_balanced_SVM'])
+
+df.to_csv(os.getcwd()+'/results/summary_results.csv')
 
 print("Gerando matrizes de confusao...")
 
@@ -120,7 +131,7 @@ for i in range(0,len(extractor_name)):
     fs.write("%s\n" % matrizC_S[i])
     fr.writelines("\n **" + extractor_name[i] + "**\n")
     fr.write("%s\n" % matrizC_R[i])
-    
+
 fs.close()
 fr.close()
 
